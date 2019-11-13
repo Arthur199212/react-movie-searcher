3a558d27216e72a524911f548f2f17054093f7e1
Artur Rubchenko <Artur_Rubchenko@epam.com>
2019-11-13 17:01:08 +0300

    refactor

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,33 +1,39 @@
-import React from "react";
-import { Typography, AppBar, Container, Link, Toolbar, Button } from "@material-ui/core";
+import React from 'react'
+import {
+  Typography,
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Button
+} from '@material-ui/core'
 
-import "./footer.scss";
+import './footer.scss'
 
-function Copyright() {
+function Copyright () {
   return (
-    <Typography variant="body2" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="#">
+    <Typography variant='body2' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='#'>
         Developer
       </Link>
       {` ${new Date().getFullYear()}.`}
     </Typography>
-  );
+  )
 }
 
 const Footer = () => {
-
   return (
     <footer>
-      <AppBar position='static' style={{padding: 0, margin: 0}}>
-        <Container maxWidth='lg' style={{padding: 0}}>
-          <Toolbar className="footer_container">
+      <AppBar position='static' style={{ padding: 0, margin: 0 }}>
+        <Container maxWidth='lg' style={{ padding: 0 }}>
+          <Toolbar className='footer_container'>
             <Copyright />
           </Toolbar>
         </Container>
       </AppBar>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
