602cf1b5a787d90e474c39c028129d597067df50
Artur Rubchenko <Artur_Rubchenko@epam.com>
2019-12-02 13:16:32 +0300

    feat: add redux-persist for savin store of redux localy

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import { HashRouter as Router } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import configureStore from './redux/store'
 
+const { store, persistor } = configureStore()
+
 render(
-  <Provider store={configureStore()}>
-    <Router>
-      <App />
-    </Router>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 )
